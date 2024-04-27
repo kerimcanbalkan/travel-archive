cc4316e4712810fe3ed72455b1ad1e6dd12f9d50
kerimcanbalkan <kerimcanbalkan@gmail.com>
2024-04-27 17:53:25 +0300

    basic interactive map complete

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        light: {
+          "fill": "#F6EEE1",
+          "text-base": "#012C56",
+          "accent": "#E14A39",
+          "card": "#D9D1C3",
+          "card-muted": "#EFD8B0",
+          "border": "#DC9891"
+        }
+      }
     },
   },
   plugins: [],
